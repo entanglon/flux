26feff5d36c5b87f710bb64ff9144bfb304164c0
zainulnazir <haditbutt7@gmail.com>
2026-04-18 21:49:34 +0530

    Add Secrets.swift

---
+++ flux/Services/Secrets.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct Secrets {
+    static let tmdbAPIKey = ""
+    static let streamRacerUrl = "https://default.com"
+    static let traktClientId = "f057ff5c2a129d2f2dabc1981883ff1098de7ee3ffb5f63cd00d23bb462de0b9"
+    static let traktClientSecret = "754db12bc4f9b5c28adce4091f0907d83cb86375c31f4a9b2b528ee1a2d59ae0"
+    static let raspberryPiStremioAddonUrl = ""
+}
